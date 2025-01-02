e8a512cf2cb29663e572d6c1f7d5e10f86fe7e70
Scott Schwab <scottschwab86@gmail.com>
2025-01-02 16:24:26 -0600

    Add signout functionality

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+import { getAuth, signOut } from "firebase/auth";
 
+const auth = getAuth();
+signOut(auth).then(() => {
+  console.log("signed out!")
+}).catch((error) => {
+  console.log(error)
+});
 </script>
 
 <template>
